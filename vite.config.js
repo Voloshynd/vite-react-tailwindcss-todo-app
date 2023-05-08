78a8ac457c8a7ac242b3e233305324da6abee9f1
Dmuyto <Voloshynd13@gmail.com>
2023-05-08 13:49:05 +0200

    Added updates

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/vite-react-tailwindcss-todo-app/",
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "/src/main.jsx",
-    },
-  },
+  // build: {
+  //   manifest: true,
+  //   rollupOptions: {
+  //     input: "/src/main.jsx",
+  //   },
+  // },
 });
